8d4b2449750c51d57ed6a2caff20b76aa6c28b12
Doug Rynar <dougrynar@gmail.com>
2022-11-16 19:06:48 -0500

    increased number of gongs to the current hour

--- core.py
+++ core.py
@@ -2,6 +2,7 @@ import os
 import discord
 from dotenv import load_dotenv 
 from discord.ext import commands
+import time
 
 def run_bot():
     load_dotenv()
@@ -11,13 +12,31 @@ def run_bot():
 
     @bot.event
     async def on_ready():
+        t = time.localtime()
+        t = time.strftime("%H:%M:%S", t)
+        hr = int(t[:2])
+        if(hr == 0):
+            hr = 12
+        if (hr > 12):
+            hr = hr - 12
 
         # To make sure this bot is only active in a particular channel in a particular server
         for guild in bot.guilds:
             if (guild.name == '\U0001d610\'\U0001d62e \U0001d62a\U0001d62f'):
                 for channel in guild.channels:
                     if channel.name == 'text_channel_mcgee':
-                        await channel.send('***GONG***')
+
+                        # once it's verified that it is sending in the correct channel,
+                        # determines how many gongs to send
+                        gongs = ""
+
+                        for x in range(hr):
+                            gongs += "***GONG*** "
+
+                        gongs = gongs[:len(gongs) - 1] # remove final space
+                        
+                        print(gongs)
+                        await channel.send(gongs)
 
         await bot.close()
 
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import core
 import schedule
+import time
 
 if(__name__ == "__main__"):
+
+    # wait for the time to be a new hour before scheduling recurring bong    
+    t = time.localtime()
+    t = time.strftime("%H:%M:%S", t)
+    initial_hr = int(t[:2])
+    hr = initial_hr
+
+    while hr == initial_hr:
+        t = time.localtime()
+        t = time.strftime("%H:%M:%S", t)
+        hr = int(t[:2])
+        time.sleep(10)        
+
     schedule.every().hour.do(core.run_bot)
+    core.run_bot()
 
     while True:
         schedule.run_pending()
